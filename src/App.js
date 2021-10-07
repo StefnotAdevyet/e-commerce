ac6019b607af478ac6a13f1b5a81a80d0645090c
stefnotadevyet <srjack91@gmail.com>
2021-10-06 21:21:06 -0400

    styled Products

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Products from "./components/Products";
+import Products from "./components/Products/Products";
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid } from '@material-ui/core';
-import Product from "./Products/Product/Product";
+import Product from "../Products/Product/Product";
+import useStyles from './styles';
 
 const products = [
   { id: 1, name: 'shoes', description: 'running shoes.', price: `$${5}`, image: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/qwqfyddzikcgc4ozwigp/revolution-5-mens-running-shoes-ZXqS6C.png'},
@@ -8,13 +9,17 @@ const products = [
 ];
 
 const Products = () => {
+  const classes = useStyles()
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
     <Grid container justify="center" spacing={4}>
       {products.map(product => {
-        return (<Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+        return (
+        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
           <Product product={product}/>
-        </Grid>)
+        </Grid>
+        )
       })}
     </Grid>
   </main>
